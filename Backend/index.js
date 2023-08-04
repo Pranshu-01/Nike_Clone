d2527662b5174acdfc58f251227a3c991d356491
unknown <pranshugoyal.cse19@chitkarauniversity.edu.in>
2023-08-04 13:58:16 +0530

    initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -14,8 +14,24 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 const cors=require('cors');
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Request-Headers', '*');
+    if (req.method === "OPTIONS") {
+      res.header('Access-Control-Allow-Methods', '*');
+      return res.status(200).json({});
+    }
+    next();
+  });
+
+
 app.use(cors({
-    origin:'*'
+    origin:'*',
+    // credentials:"true",
+    // allowedHeaders: ['Content-Type'],
+    // exposedHeaders: ['Content-Type']
 }));
 
 const dotenv=require('dotenv');
